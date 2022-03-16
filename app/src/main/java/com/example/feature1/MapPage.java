eff79241a6589fc8fe4b2f7e9af9036c592de9d1
Luis Contreras <luisc@usc.edu>
2022-03-15 19:46:59 -0700

    user can now see his info upon logging in

--- app/src/main/java/com/example/feature1/MapPage.java
+++ app/src/main/java/com/example/feature1/MapPage.java
@@ -1,23 +1,52 @@
 package com.example.feature1;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
 
 public class MapPage extends AppCompatActivity {
 
     private Button elogout;
+    TextView name, email, studentID;
+    FirebaseAuth fAuth;
+    FirebaseFirestore fStore;
+    String userId;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map_page);
 
         elogout = findViewById(R.id.LogoutButton);
+
+        name = findViewById(R.id.TVname);
+        email = findViewById(R.id.TVemail);
+        studentID = findViewById(R.id.TVstudentID);
+
+        fAuth = FirebaseAuth.getInstance();
+        fStore = FirebaseFirestore.getInstance();
+        userId = fAuth.getCurrentUser().getUid();
+
+        DocumentReference documentReference = fStore.collection("users").document(userId);
+        documentReference.addSnapshotListener(this, new EventListener<DocumentSnapshot>() {
+            @Override
+            public void onEvent(@Nullable DocumentSnapshot value, @Nullable FirebaseFirestoreException error) {
+                name.setText(value.getString("name"));
+                email.setText(value.getString("email"));
+                studentID.setText(value.getString("studentID"));
+            }
+        });
 
         elogout.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -27,13 +56,7 @@ public class MapPage extends AppCompatActivity {
                 finish();
             }
         });
-    }
 
 
-    public void logout(View view)
-    {
-        FirebaseAuth.getInstance().signOut();
-        startActivity(new Intent(getApplicationContext(), LoginActivity.class));
-        finish();
     }
 }
--- app/src/main/java/com/example/feature1/RegistrationActivity.java
+++ app/src/main/java/com/example/feature1/RegistrationActivity.java
@@ -6,24 +6,35 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class RegistrationActivity extends AppCompatActivity {
 
-    private EditText eRegName, eRegPassword;
+    public static final String TAG = "TAG";
+    private EditText eRegName, eRegPassword, eFullName, ePhotoURL, eStudentID;
     private Button eRegister;
 
     public static Credentials credentials;
 
     FirebaseAuth fAuth;
+    FirebaseFirestore fStore;
+
+    String userID;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,19 +43,20 @@ public class RegistrationActivity extends AppCompatActivity {
         eRegName = findViewById(R.id.etRegName);
         eRegPassword = findViewById(R.id.etRegPassword);
         eRegister = findViewById(R.id.btnRegister);
+        eFullName = findViewById(R.id.etFullName);
+        ePhotoURL = findViewById(R.id.etPhotoURL);
+        eStudentID = findViewById(R.id.etStudentID);
 
         fAuth = FirebaseAuth.getInstance();
-
-//        if(fAuth.getCurrentUser() != null)
-//        {
-//            startActivity(new Intent(RegistrationActivity.this, MainActivity.class));
-//            finish();
-//        }
+        fStore = FirebaseFirestore.getInstance();
         eRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String regUsername = eRegName.getText().toString();
-                String regPassword = eRegPassword.getText().toString();
+                final String regUsername = eRegName.getText().toString();
+                final String regPassword = eRegPassword.getText().toString();
+                final String regFullName = eFullName.getText().toString();
+                final String regPhotoURL = ePhotoURL.getText().toString();
+                final String regStudentID = eStudentID.getText().toString();
 
                 if(validate(regUsername, regPassword))
                 {
@@ -54,6 +66,21 @@ public class RegistrationActivity extends AppCompatActivity {
                             if(task.isSuccessful())
                             {
                                 Toast.makeText(RegistrationActivity.this, "Registration successful", Toast.LENGTH_SHORT).show();
+                                userID = fAuth.getCurrentUser().getUid();
+                                DocumentReference documentReference = fStore.collection("users").document(userID);
+                                Map<String, Object> user = new HashMap<>();
+                                user.put("name", regFullName);
+                                user.put("email", regUsername);
+                                user.put("password", regPassword);
+                                user.put("photoURL", regPhotoURL);
+                                user.put("studentID", regStudentID);
+
+                                documentReference.set(user).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                    @Override
+                                    public void onSuccess(Void unused) {
+                                        Log.d(TAG, "onSuccess: user Profile is created for " + userID);
+                                    }
+                                });
                                 startActivity(new Intent(RegistrationActivity.this, MapPage.class));
                             }
                             else
