11630bf579cb468a140e491fd73f102895e29986
Alexander Rouse <acrouse@usc.edu>
2022-04-09 14:24:00 -0700

    Testing example class

---
+++ app/src/test/java/com/example/feature1/Feature3WhiteBoxTests.java
@@ -0,0 +1,14 @@
+package com.example.feature1;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class Feature3WhiteBoxTests
+{
+        @Test
+        public void addition_isCorrect() {
+            assertEquals(4, 2 + 2);
+            System.out.println("Hello there");
+    }
+}
