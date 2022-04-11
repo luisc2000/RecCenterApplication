8758f3ab93416e5c6d6ff71876bf2b9c216b2209
Luis Contreras <luisc@usc.edu>
2022-04-11 00:13:57 -0700

    Finished Feature1 BlackBox testing

--- app/src/androidTest/java/com/example/feature1/Feature1BlackBoxTests.java
+++ app/src/androidTest/java/com/example/feature1/Feature1BlackBoxTests.java
@@ -1,12 +1,14 @@
 package com.example.feature1;
 
 import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.scrollTo;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isClickable;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
-
-import androidx.test.espresso.ViewAction;
 import androidx.test.espresso.action.ViewActions;
 import androidx.test.espresso.matcher.ViewMatchers;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
@@ -17,6 +19,7 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+
 @RunWith(AndroidJUnit4.class)
 @LargeTest
 public class Feature1BlackBoxTests {
@@ -26,20 +29,79 @@ public class Feature1BlackBoxTests {
             new ActivityScenarioRule<>(LoginActivity.class);
 
     @Test
-    public void testLogin() {
+    public void testLogin() throws InterruptedException {
         String username = "Bell@usc.edu", password = "JonBell";
         onView(withText("New User? Click Me to Sign Up")).check(matches(isDisplayed()));
         onView(ViewMatchers.withId(R.id.etName)).perform(ViewActions.typeText(username));
         onView(ViewMatchers.withId(R.id.edPassword)).perform(ViewActions.typeText(password));
-        onView(withId(R.id.btnLogin)).perform(ViewActions.click());
+        onView(withId(R.id.btnLogin)).perform(click());
+        Thread.sleep(2600);
+//        onView(withId(R.id.LogoutButton)).perform(closeSoftKeyboard());
+//        onView(withId(R.id.LogoutButton)).perform(click());
     }
 
+    @Test
+    public void testLoginNonExistent() throws InterruptedException { // logs in with user information that does not exists
+        String username = "fake@usc.edu", password = "fake";
+        onView(ViewMatchers.withId(R.id.etName)).perform(ViewActions.typeText(username));
+        onView(ViewMatchers.withId(R.id.edPassword)).perform(ViewActions.typeText(password));
+        onView(withId(R.id.btnLogin)).perform(click());
+        Thread.sleep(500);
+    }
+
+    @Test
+    public void testMap() throws InterruptedException {
+        String username = "Bell@usc.edu", password = "JonBell";
+        onView(withText("New User? Click Me to Sign Up")).check(matches(isDisplayed()));
+        onView(ViewMatchers.withId(R.id.etName)).perform(ViewActions.typeText(username));
+        onView(ViewMatchers.withId(R.id.edPassword)).perform(ViewActions.typeText(password));
+        onView(withId(R.id.btnLogin)).perform(click());
+        Thread.sleep(6000);
+
+        onView(ViewMatchers.withId(R.id.map)).perform(ViewActions.swipeLeft());
+        Thread.sleep(3000);
+        onView(ViewMatchers.withId(R.id.map)).perform(ViewActions.swipeRight());
+        Thread.sleep(1400);
+
+        onView(ViewMatchers.withId(R.id.map)).perform(ViewActions.swipeRight());
+        Thread.sleep(2000);
+        onView(ViewMatchers.withId(R.id.map)).perform(ViewActions.swipeLeft());
+        Thread.sleep(2000);
+    }
 //    @Test
-//    public void testNewUser() {
-//        String username = "Test@usc.edu", password = "JonBell";
-//        onView(withText("New User? Click Me to Sign Up")).check(matches(isDisplayed()));
-//        onView(ViewMatchers.withId(R.id.etName)).perform(ViewActions.typeText(username));
-//        onView(ViewMatchers.withId(R.id.edPassword)).perform(ViewActions.typeText(password));
-//        onView(withId(R.id.btnLogin)).perform(ViewActions.click());
+//    public void testNewUser() { // this test creates a new user
+//        onView(withId(R.id.tvRegister)).perform(ViewActions.click());
+//        String name = "Your Name Here", email = "email@usc.edu", password = "RandomPassword", id = "123456", URL = "url.com";
+//        onView(ViewMatchers.withId(R.id.etFullName)).perform(ViewActions.typeText(name));
+//        onView(ViewMatchers.withId(R.id.etRegName)).perform(ViewActions.typeText(email));
+//        onView(ViewMatchers.withId(R.id.etStudentID)).perform(ViewActions.typeText(id));
+//        onView(ViewMatchers.withId(R.id.etRegPassword)).perform(ViewActions.typeText(password));
+//        onView(ViewMatchers.withId(R.id.etPhotoURL)).perform(ViewActions.typeText(URL));
+//        onView(withId(R.id.btnRegister)).perform(ViewActions.click());
 //    }
+
+    @Test
+    public void testRecCenters() throws InterruptedException {
+        String username = "Bell@usc.edu", password = "JonBell";
+        onView(withText("New User? Click Me to Sign Up")).check(matches(isDisplayed()));
+        onView(ViewMatchers.withId(R.id.etName)).perform(ViewActions.typeText(username));
+        onView(ViewMatchers.withId(R.id.edPassword)).perform(ViewActions.typeText(password));
+        onView(withId(R.id.btnLogin)).perform(click());
+        Thread.sleep(6000);
+        onView(withId(R.id.xmlLyonButton)).perform(click());
+        Thread.sleep(2000);
+        onView(withId(R.id.backBtn_feature2)).perform(click());
+
+        onView(withId(R.id.xmlCromwellButton)).perform(click());
+        Thread.sleep(2000);
+        onView(withId(R.id.backBtn_feature2)).perform(click());
+
+        onView(withId(R.id.xmlUytengsuButton)).perform(click());
+        Thread.sleep(2000);
+        onView(withId(R.id.backBtn_feature2)).perform(click());
+
+        onView(withId(R.id.xmlVillageButton)).perform(click());
+        Thread.sleep(2000);
+        onView(withId(R.id.backBtn_feature2)).perform(click());
+    }
 }
